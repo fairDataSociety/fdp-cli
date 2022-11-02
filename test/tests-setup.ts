cd97901acc24f904e31015500869af9b9f781cf4
Igor Shadurin <igor.shadurin@gmail.com>
2022-11-02 12:33:04 +0300

    refactor: check whether the environment variable available

--- test/tests-setup.ts
+++ test/tests-setup.ts
@@ -2,5 +2,5 @@ import { createUsableBatch } from './utils'
 
 export default async function testsSetup(): Promise<void> {
   // internal env param for caching batch id
-  process.env.CACHED_BEE_BATCH_ID = await createUsableBatch()
+  process.env.CACHED_BEE_BATCH_ID = process.env.CACHED_BEE_BATCH_ID || (await createUsableBatch())
 }
