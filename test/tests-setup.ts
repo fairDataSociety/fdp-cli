a7b94917a3ac3d3bb1bc4e6849212c23365ae6ac
Igor Shadurin <igor.shadurin@gmail.com>
2022-11-04 16:22:31 +0300

    refactor: display batch id

--- test/tests-setup.ts
+++ test/tests-setup.ts
@@ -2,5 +2,11 @@ import { createUsableBatch } from './utils'
 
 export default async function testsSetup(): Promise<void> {
   // internal env param for caching batch id
-  process.env.CACHED_BEE_BATCH_ID = process.env.CACHED_BEE_BATCH_ID || (await createUsableBatch())
+  if (!process.env.CACHED_BEE_BATCH_ID) {
+    process.env.CACHED_BEE_BATCH_ID = await createUsableBatch()
+  }
+
+  console.log(
+    `\nBatch ID will be used: ${process.env.CACHED_BEE_BATCH_ID}. It is stored in 'process.env.CACHED_BEE_BATCH_ID'\n`,
+  )
 }
