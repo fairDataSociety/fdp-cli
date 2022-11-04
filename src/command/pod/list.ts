8f34f1b4b2f8f2d0a0ebae6b059b6da354fc8a3a
Igor Shadurin <igor.shadurin@gmail.com>
2022-11-04 18:32:46 +0300

    refactor: display pods type

--- src/command/pod/list.ts
+++ src/command/pod/list.ts
@@ -9,14 +9,25 @@ export class List extends PodCommand implements LeafCommand {
 
   public readonly description = 'List pods'
 
+  /**
+   * Shows pod information
+   */
+  private showPodInfo(podName: string, podType: string): void {
+    this.console.log(createKeyValue('Name', podName))
+    this.console.log(createKeyValue('Type', podType))
+    this.console.divider()
+  }
+
   public async run(): Promise<void> {
     await super.init()
 
     await this.fillFdpAccount(this.account, this.password)
     const pods = await this.fdpStorage.personalStorage.list()
     for (const pod of [...pods.getPods(), ...pods.getSharedPods()]) {
-      this.console.log(createKeyValue('Name', pod.name))
-      this.console.divider()
+      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+      // @ts-ignore
+      const isSharedPod = Boolean((pod as unknown).address)
+      this.showPodInfo(pod.name, isSharedPod ? 'shared pod' : 'pod')
     }
   }
 }
