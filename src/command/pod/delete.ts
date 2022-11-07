86881ab3729703653091bc4a042614f8c70257bb
Igor Shadurin <igor.shadurin@gmail.com>
2022-11-07 14:03:16 +0300

    feat: added ability to force delete

--- src/command/pod/delete.ts
+++ src/command/pod/delete.ts
@@ -1,7 +1,8 @@
-import { Argument, LeafCommand } from 'furious-commander'
+import { Argument, LeafCommand, Option } from 'furious-commander'
 import { Message } from '../../utils/message'
 import { createKeyValue } from '../../utils/text'
 import { PodCommand } from './pod-command'
+import { exit } from 'process'
 
 export class Delete extends PodCommand implements LeafCommand {
   public readonly name = 'delete'
@@ -11,12 +12,34 @@ export class Delete extends PodCommand implements LeafCommand {
   @Argument({ key: 'name', default: 'main', description: 'Reference name of the pod' })
   public podName!: string
 
+  @Option({
+    type: 'boolean',
+    key: 'force',
+    alias: 'f',
+    description: 'Force deletion without confirmation',
+  })
+  public force!: boolean
+
   public async run(): Promise<void> {
     await super.init()
 
     await this.fillFdpAccount(this.account, this.password)
+    await this.promptPodDeletion()
     await this.fdpStorage.personalStorage.delete(this.podName)
     this.console.log(Message.podDeletedSuccessfully())
     this.console.log(createKeyValue('Name', this.podName))
+  }
+
+  /**
+   * Prompts warning about pod deletion
+   */
+  private async promptPodDeletion(): Promise<void> {
+    if (this.force || this.yes) {
+      return
+    }
+
+    if (!(await this.console.confirmAndDelete('This action will delete the pod without ability to recover. Delete?'))) {
+      exit(0)
+    }
   }
 }
