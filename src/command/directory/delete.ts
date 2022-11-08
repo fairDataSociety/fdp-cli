e173698bf724a5b1e27fb789ad6d95bbaa554f31
Igor Shadurin <igor.shadurin@gmail.com>
2022-11-08 19:18:28 +0300

    fix: incorrect method calling

--- src/command/directory/delete.ts
+++ src/command/directory/delete.ts
@@ -10,7 +10,7 @@ export class Delete extends DirectoryCommand implements LeafCommand {
   public async run(): Promise<void> {
     await super.init()
 
-    await this.fillFdpAccount(this.account, this.password)
+    await this.setFdpAccount(this.account, this.password)
     await this.fdpStorage.directory.delete(this.pod, this.path)
     this.console.log(Message.directoryDeletedSuccessfully(this.path))
   }
